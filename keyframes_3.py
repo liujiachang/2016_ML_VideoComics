ef0fa5a247d4db0edf2802cee36e77b19faaccef
liujiachang <1040496263@qq.com>
2019-06-04 19:15:57 +0800

    kmeans修改

--- keyframes_3.py
+++ keyframes_3.py
@@ -10,23 +10,24 @@ if not os.path.exists(frame_save_path):
 
 frames = os.listdir(frame_src_path)
 
-X=[]
+X = np.zeros([len(frames),64])
 for i in range(len(frames)):
     print("---> 正在处理第%d张图片:" % i)
-    index = index + 1
     img1 = cv2.imread(frame_src_path + '/' + str(i) + '.jpg')
-    img1_new = cv2.resize(img1, (8, 8), interpolation=cv2.INTER_AREA)
-    img1_new = cv2.cvtColor(img1_new, cv2.COLOR_BGR2GRAY)
+    img1_new = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
+    img1_new = cv2.resize(img1_new, (8, 8), interpolation=cv2.INTER_AREA)
     # 灰度化处理
     img = np.array(img1_new)
-    img = img.reshape(-1).tolist()
-    X.append(img)
+    img = img.reshape(-1)
+    X[i] = img
+
 #print(X)
-kmeans=KMeans(n_clusters=10)
+k = 10 # 聚类数量
+kmeans=KMeans(n_clusters=k)
 kmeans.fit(X)
-Y=[i for i in range(len(kmeans.cluster_centers_))]
+Y=[i for i in range(k)]
 #print(kmeans.cluster_centers_)
-for i in range(len(kmeans.cluster_centers_)):
+for i in range(k):
     m=99999
     for j in range(len(X)):
         img = np.array(X[j])
@@ -35,36 +36,6 @@ for i in range(len(kmeans.cluster_centers_)):
             m=t
             Y[i]=j
 
-k = 10 #聚类 k 取值
-frame_src_path = "datasets/frames"
-frame_save_path = "datasets/keyframes"
-os.mkdir(frame_save_path)#创建关键帧目录
-frames_1 = os.listdir(frame_src_path)#进入目录
-
-X = np.zeros([1,720*480])
-for i in range(len(frames_1)):
-    print("正在读取第",i,"张图片---：")
-    img = cv2.imread(frame_src_path + '/' + str(i) + '.jpg')  # 读取图片
-    img = cv2.resize(img, (8, 8), interpolation=cv2.INTER_AREA)
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)  # RGB灰度化处理
-    img = np.array(img)
-    img = img.ravel()
-    if i==0:
-        X = img
-    else:
-        X = np.vstack((X,img))
-print("正在进行聚类----：")
-y = KMeans(n_clusters = k).fit(X)
-Y=[i for i in range(len(y.cluster_centers_))]
-print("寻找中心点----：")
-for i in range(len(y.cluster_centers_)):
-    D = 999999
-    for j in range(len(y.labels_)):
-        d = np.sqrt(np.sum(np.power(X[j]-y.cluster_centers_[i],2)))
-        if d < D:
-            D = d
-            #中心点数组保存中心点的图片编号
-            Y[i] = j
 Y.sort()
 print(Y)#打印一下图片编号数组
 #图片保存：
